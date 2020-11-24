a8fec79715a750da80989c75135cb9a87735a72e
alaabashiyi <alaa.bashiy@gmail.com>
2020-11-24 17:41:23 +0200

    my changes

---
+++ src/api/api.js
@@ -0,0 +1,4 @@
+export const getPokemonsApi = (filter) => {
+  const data = fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20");
+  return data;
+};
--- src/screens/App/App.js
+++ src/screens/App/App.js
@@ -1,9 +1,28 @@
 import "./App.css";
+import Pokeprofile from "../../components/Pokeprofile/Pokeprofile";
+import { getPokemonsApi } from "../../api/api";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [pokeData, setPokeData] = useState(null);
+
+  const getPokemons = () => {
+    getPokemonsApi()
+      .then((res) => res.json())
+      .then((res) => {
+        setPokeData(res);
+        console.log(pokeData);
+      })
+      .catch((e) => console.log(e));
+  };
+
+  useEffect(() => {
+    getPokemons();
+  }, []);
   return (
     <div className="App">
       <span>HELLLO</span>
+      <Pokeprofile />
     </div>
   );
 }
