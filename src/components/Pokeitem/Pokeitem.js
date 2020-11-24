d8e9fe70118d2ab60b3e1f6cd294520d4bee2de5
shoogkabiya <shoogkabiya@hotmail.com>
2020-11-24 21:38:52 +0200

    adition6

--- src/components/Pokeitem/Pokeitem.js
+++ src/components/Pokeitem/Pokeitem.js
@@ -6,8 +6,8 @@ function Pokeitem(props) {
   const { objectpokemon, index } = props;
   return (
     <div className="Pokeitem">
-      <h2>{objectpokemon.name}</h2>
       <img src={`${imgUrl}${index}.png`} />
+      <h2>{objectpokemon.name}</h2>
     </div>
   );
 }
