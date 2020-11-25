b4fbcbe394dcbcbad71fefb49a61416d02be1f76
nuwrss <nuwrssazzam@gmail.com>
2020-11-25 10:10:24 +0200

    ttata

--- src/components/Pokeitem/Pokeitem.js
+++ src/components/Pokeitem/Pokeitem.js
@@ -3,6 +3,7 @@ const imgUrl =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
 function Pokeitem(props) {
   const { objectpokemon, index } = props;
+  console.log(props);
   return (
     <div className="Pokeitem">
       <h2>{objectpokemon.name}</h2>
